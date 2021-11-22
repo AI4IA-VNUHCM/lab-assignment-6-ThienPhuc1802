93d242223f0929aa12906f8c42a0482b348f0130
ThienPhuc1802 <91868450+ThienPhuc1802@users.noreply.github.com>
2021-11-22 15:00:58 +0700

    ex1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -21,27 +21,45 @@ void Ex1(int n){
   	int a = n-d*1000-b*10-c*100;
  
     switch(d){
-      case 1: printf("one thousand ");break;
-      case 2: printf("two thousand ");break;
-      case 3: printf("three thousand ");break;
-      case 4: printf("four thousand ");break;
-      case 5: printf("five thousand ");break;
-      case 6: printf("six thousand ");break;
-      case 7: printf("seven thousand ");break;
-      case 8: printf("eight thousand ");break;
-      case 9: printf("nine thousand ");break;
+      case 1: printf("one thousand ");
+      break;
+      case 2: printf("two thousand ");
+      break;
+      case 3: printf("three thousand ");
+      break;
+      case 4: printf("four thousand ");
+      break;
+      case 5: printf("five thousand ");
+      break;
+      case 6: printf("six thousand ");
+      break;
+      case 7: printf("seven thousand ");
+      break;
+      case 8: printf("eight thousand ");
+      break;
+      case 9: printf("nine thousand ");
+      break;
     }
   
     switch(c){
-      case 1: printf("one hundred ");break;
-      case 2: printf("two hundred ");break;
-      case 3: printf("three hundred ");break;
-      case 4: printf("four hundred ");break;
-      case 5: printf("five hundred ");break;
-      case 6: printf("six hundred ");break;
-      case 7: printf("seven hundred ");break;
-      case 8: printf("eight hundred ");break;
-      case 9: printf("nine hundred ");break;
+      case 1: printf("one hundred ");
+      break;
+      case 2: printf("two hundred ");
+      break;
+      case 3: printf("three hundred ");
+      break;
+      case 4: printf("four hundred ");
+      break;
+      case 5: printf("five hundred ");
+      break;
+      case 6: printf("six hundred ");
+      break;
+      case 7: printf("seven hundred ");
+      break;
+      case 8: printf("eight hundred ");
+      break;
+      case 9: printf("nine hundred ");
+      break;
     }
     
     
@@ -91,27 +109,44 @@ void Ex1(int n){
 	  
 	  
 	  break;
-      case 2: printf("twenty ");break;
-      case 3: printf("thirty ");break;
-      case 4: printf("forty ");break;
-      case 5: printf("fifty ");break;
-      case 6: printf("sixty ");break;
-      case 7: printf("seventy ");break;
-      case 8: printf("eighty ");break;
-      case 9: printf("ninty ");break;
+      case 2: printf("twenty ");
+      break;
+      case 3: printf("thirty ");
+      break;
+      case 4: printf("forty ");
+      break;
+      case 5: printf("fifty ");
+      break;
+      case 6: printf("sixty ");
+      break;
+      case 7: printf("seventy ");
+      break;
+      case 8: printf("eighty ");
+      break;
+      case 9: printf("ninty ");
+      break;
     }
 	
    if (b!=1){
     switch(a){
-      case 1: printf("one ");break;
-      case 2: printf("two ");break;
-      case 3: printf("three ");break;
-      case 4: printf("four ");break;
-      case 5: printf("five");break;
-      case 6: printf("six ");break;
-      case 7: printf("seven ");break;
-      case 8: printf("eight ");break;
-      case 9: printf("nine ");break;
+      case 1: printf("one ");
+      break;
+      case 2: printf("two ");
+      break;
+      case 3: printf("three ");
+      break;
+      case 4: printf("four ");
+      break;
+      case 5: printf("five");
+      break;
+      case 6: printf("six ");
+      break;
+      case 7: printf("seven ");
+      break;
+      case 8: printf("eight ");
+      break;
+      case 9: printf("nine ");
+      break;
     }
    }
 }
